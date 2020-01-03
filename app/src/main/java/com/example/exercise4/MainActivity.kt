55bf9bb500617280c6318bda3d4915ebc8387fbb
yen khai <yenkhai.99@hotmail.com>
2020-01-03 20:14:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/exercise4/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.exercise4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var calendar:Calendar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Set a click listener for button widget
+        button.setOnClickListener{
+            // Initialize a new DatePickerFragment
+            val newFragment = DatePickerFragment()
+            // Show the date picker dialog
+            newFragment.show(fragmentManager, "Date Picker")
+
+        }
+
+        Calculate.setOnClickListener{
+            val age = editText.text.toString().toInt()
+            val basicSaving = getSaving(age) * 30 / 100;
+
+            editText2.text = "Basic Saving : RM"+getSaving(age)
+            textView4 .text = " Result: RM"+basicSaving;
+
+        }
+    }
+
+    fun getSaving(age:Int) : Double{
+        when(age){
+            in 16..20 -> return 5000.00
+            in 20..25 -> return 14000.00
+            in 26..30 -> return 29000.00
+            in 31..35 -> return 50000.00
+            else -> return 0.00
+        }
+    }
+}
